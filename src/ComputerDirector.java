99b5b704bc562a1f3db6c5d9c8aee565fce6f77e
Berik Mukhametyarov <amirromanov58@gmail.com>
2026-09-13 21:55:12 +0500

    Director is created

--- src/ComputerDirector.java
+++ src/ComputerDirector.java
@@ -1,2 +1,22 @@
 public class ComputerDirector {
+
+    public Computer buildGamingComputer(ComputerBuilder builder) {
+        return builder
+                .setCpu("Intel i9")
+                .setRam(32)
+                .setStorage("1TB SSD")
+                .setGpu("RTX 4070")
+                .setOperatingSystem("Windows 11")
+                .build();
+    }
+
+    public Computer buildOfficeComputer(ComputerBuilder builder) {
+        return builder
+                .setCpu("Intel i3")
+                .setRam(8)
+                .setStorage("512GB SSD")
+                .setGpu("Integrated Graphics")
+                .setOperatingSystem("Windows 11")
+                .build();
+    }
 }
