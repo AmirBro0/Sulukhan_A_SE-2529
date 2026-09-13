6074c8176ffc33174a4c3304eea066156a29ac82
Berik Mukhametyarov <amirromanov58@gmail.com>
2026-09-13 21:56:55 +0500

    Client is created(Main). Not official realase(w/o Readme etc.)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
-void main() {
+public class Main {
+    public static void main(String[] args) {
+        ComputerDirector director = new ComputerDirector();
 
+        Computer gamingComputer =
+                director.buildGamingComputer(new GamingComputerBuilder());
+
+        Computer officeComputer =
+                director.buildOfficeComputer(new OfficeComputerBuilder());
+
+        System.out.println("Gaming computer:");
+        System.out.println(gamingComputer);
+
+        System.out.println("\nOffice computer:");
+        System.out.println(officeComputer);
+    }
 }
